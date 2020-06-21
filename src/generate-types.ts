b99f0c9c61b87e690d98dca3ca0b221e3d91e029
Dave Campion <me@davecampion.com>
2020-06-21 15:07:48 -0700

    cut old cruft

--- src/generate-types.ts
+++ src/generate-types.ts
@@ -1,17 +1,11 @@
 import { parseAndTraverse } from "./traverse-entry";
 import { runtypeVisitor, VisitorState } from "./runtype-visitor";
-import { changeGraph } from "./change-graph";
 import {createRegistry} from "./registry";
 import generate from '@babel/generator';
 import {format} from 'prettier';
 
 export const generateTypes = (code: string) => {
   const initialState: VisitorState = {
-    output: "",
-    namespace: new Set<string>(),
-    changeGraph: changeGraph(),
-    shapes: new Map<string, [string, Record<string, string>]>(),
-    astNodes: [],
     registry: createRegistry(),
   };
 
--- src/runtype-visitor.ts
+++ src/runtype-visitor.ts
@@ -11,79 +11,47 @@ import {
   Literal,
   ObjectExpression,
   ObjectProperty,
-  Node,
 } from "@babel/types";
-import { format as prettierFormat } from "prettier";
-import { equals } from "ramda";
-import { formatRecord, merge, toProperCase } from "./util";
-import { ChangeGraph } from "./change-graph";
 import { CompileType } from "./type-creator";
 import {Registry} from "./registry";
 
 export interface VisitorState {
-  output: string;
-  namespace: Set<string>;
-  shapes: Map<string, [string, Record<string, string>]>;
-  changeGraph: ChangeGraph;
-  astNodes: Node[];
   registry: Registry;
 }
 
 interface TypeDeclarations {
-  type?: string;
   compileType?: CompileType;
 }
 
 const isValidObjectProp = ({ computed }: ObjectProperty) => !computed;
 
 const getTypeFromFunctionExpression = (_node: Function) : TypeDeclarations => ({
-  type: `RT.Function`,
   compileType: {
     type: "function",
   },
 });
 
-const getSafeName = (
-  namespace: Set<string>,
-  baseName: string,
-  check = 0
-): string => {
-  const checkName = check === 0 ? baseName : `${baseName}${check}`;
-  const safeName = /^[a-zA-Z]/.test(checkName) ? checkName : `Prop${checkName}`;
-
-  if (namespace.has(safeName))
-    return getSafeName(namespace, baseName, Number(check) + 1);
-
-  namespace.add(safeName);
-
-  return safeName;
-};
-
 const getTypeFromLiteral = (node: Literal): TypeDeclarations => {
   if (isStringLiteral(node) || isTemplateLiteral(node)) {
     return {
-      type: `RT.String`,
       compileType: {
         type: "string",
       },
     };
   } else if (isNumericLiteral(node)) {
     return {
-      type: `RT.Number`,
       compileType: {
         type: "number",
       },
     };
   } else if (isBooleanLiteral(node)) {
     return {
-      type: `RT.Boolean`,
       compileType: {
         type: "boolean",
       },
     };
   } else if (isNullLiteral(node)) {
     return {
-      type: `RT.Null`,
       compileType: {
         type: "null",
       },
@@ -126,7 +94,7 @@ const extractSchemas = (
     );
   }
 
-  return { type: undefined };
+  return {};
 };
 
 const reduceElements = (
@@ -136,11 +104,11 @@ const reduceElements = (
 ) =>
   els.reduce<{ types: Set<string>; collection: TypeDeclarations[] }>(
     ({ types, collection }, path) => {
-      const { type, compileType } = extractSchemas(path, state, parentKey);
+      const { compileType } = extractSchemas(path, state, parentKey);
 
-      if (type) {
-        types.add(type);
-        collection.push({ type, compileType });
+      if (compileType) {
+        types.add(JSON.stringify(compileType));
+        collection.push({ compileType });
       }
 
       return { types, collection };
@@ -159,18 +127,16 @@ const generateCollection = (
     parentKey
   );
 
-  if (collection.length === 0) return { type: undefined };
+  if (collection.length === 0) return {};
 
   return types.size === 1
     ? {
-        type: `RT.Array(${collection[0].type!})`,
         compileType: {
           type: "array",
           expr: collection[0].compileType!,
         },
       }
     : {
-        type: `RT.Tuple(${collection.map(({type}) => type!).join(", ")})`,
         compileType: {
           type: "tuple",
           params: collection.map(({compileType}) => compileType!),
@@ -182,8 +148,7 @@ const collectRecordFromExpression = (
   oExp: NodePath<ObjectExpression>,
   state: VisitorState
 ) => {
-  const record: Record<string, string> = {};
-  const betterRecord : Record<string, CompileType> = {};
+  const record : Record<string, CompileType> = {};
 
   oExp.traverse({
     ObjectProperty(path) {
@@ -191,18 +156,17 @@ const collectRecordFromExpression = (
       const key = extractPropertyName(path);
       const value = path.get("value");
 
-      const { type, compileType } = extractSchemas(value, state, key);
+      const { compileType } = extractSchemas(value, state, key);
 
-      if (!type) return;
+      if (!compileType) return;
 
-      record[key] = type;
-      betterRecord[key] = compileType!;
+      record[key] = compileType!;
 
       path.skip();
     },
   });
 
-  return {record, betterRecord};
+  return {record};
 };
 
 const generateRecord = (
@@ -210,63 +174,16 @@ const generateRecord = (
   oExp: NodePath<ObjectExpression>,
   state: VisitorState
 ): TypeDeclarations => {
-  const {record, betterRecord} = collectRecordFromExpression(oExp, state);
+  const {record} = collectRecordFromExpression(oExp, state);
 
-  state.registry.add(id, betterRecord);
-
-  const recordSignature = Object.keys(record).join(",");
-
-  if (state.shapes.has(recordSignature)) {
-    const [existingTypename, existingRecord] = state.shapes.get(
-      recordSignature
-    )!;
-
-    const merged = merge(existingRecord, record);
-
-    if (equals(existingRecord, merged)) {
-      return {
-        type: existingTypename,
-        compileType: {
-          type: "ident",
-          name: existingTypename
-        }
-      };
-    }
-
-    const newName =
-      toProperCase(id) === existingTypename
-        ? existingTypename
-        : getSafeName(
-            state.namespace,
-            `${toProperCase(id)}${existingTypename}`
-          );
-
-    if (newName !== existingTypename) {
-      state.changeGraph.add(existingTypename, newName);
-    }
-
-    state.shapes.set(recordSignature, [newName, merged]);
-
-    return {
-      type: newName,
-      compileType: {
-        type: "ident",
-        name: newName
-      }
-    };
-  }
-
-  const typeName = getSafeName(state.namespace, toProperCase(id));
-
-  state.shapes.set(recordSignature, [typeName, record]);
+  const name = state.registry.add(id, record);
 
   return {
-    type: typeName,
     compileType: {
-      type: "ident",
-      name: typeName
+      type : "ident",
+      name
     }
-  };
+  }
 };
 
 export const runtypeVisitor: Visitor<VisitorState> = {
@@ -279,22 +196,4 @@ export const runtypeVisitor: Visitor<VisitorState> = {
       generateRecord(id.node.name, init, state);
     }
   },
-};
-
-export const formatState = ({ shapes, output, changeGraph }: VisitorState) => {
-  const schemas = Array.from(shapes.values()).reduce(
-    (output, [typeName, record]) => {
-      const ident = changeGraph.resolve(typeName);
-      const schema = `export const ${ident} = RT.Record(${formatRecord(
-        record,
-        changeGraph
-      )});`;
-      const type = `export type ${ident} = RT.Static<typeof ${typeName}>;`;
-
-      return `${output}${schema}\n${type}\n\n`;
-    },
-    ""
-  );
-
-  return prettierFormat(output + schemas, { parser: "babel" });
 };
